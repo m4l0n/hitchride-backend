7ec94169f7836ff765504d2f884f1a4fcf27f157
Nigel <tp062253@mail.apu.edu.my>
2023-06-24 18:01:23 +0800

    added new fields and change field naming convention

--- src/main/java/com/m4l0n/hitchride/pojos/User.java
+++ src/main/java/com/m4l0n/hitchride/pojos/User.java
@@ -1,19 +1,24 @@
 package com.m4l0n.hitchride.pojos;
 
+import com.google.cloud.firestore.GeoPoint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
 
-    public String id;
-    public String name;
-    public String email;
-    public String password;
-    public String phoneNumber;
-    public String photoUrl;
-    public Integer points;
+    public String userId;
+    public String userName;
+    public String userEmail;
+    public String userPhoneNumber;
+    public String userPhotoUrl;
+    public Integer userPoints;
+    public Map<String, GeoPoint> userSavedLocations;
+    public DriverInfo userDriverInfo;
+
 }
