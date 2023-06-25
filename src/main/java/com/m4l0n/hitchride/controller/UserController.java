252d6e8a3cce9733650bbfc55af9a8b9059624c3
Nigel Ang <67460463+m4l0n@users.noreply.github.com>
2023-06-25 14:54:30 +0800

    added missing imports in usercontroller

--- src/main/java/com/m4l0n/hitchride/controller/UserController.java
+++ src/main/java/com/m4l0n/hitchride/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.m4l0n.hitchride.controller;
 
+import com.google.cloud.firestore.GeoPoint;
 import com.m4l0n.hitchride.exceptions.HitchrideException;
 import com.m4l0n.hitchride.pojos.User;
 import com.m4l0n.hitchride.response.Response;
@@ -8,6 +9,9 @@ import com.m4l0n.hitchride.service.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
 
 @RestController
 @SecurityRequirement(name = "bearerAuth")
