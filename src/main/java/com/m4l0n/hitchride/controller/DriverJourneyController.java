1c293dfe50878629acec81daa373331ac6aeeb0b
Nigel <tp062253@mail.apu.edu.my>
2023-06-25 17:41:12 +0800

    add api to create new driver journey

---
+++ src/main/java/com/m4l0n/hitchride/controller/DriverJourneyController.java
@@ -0,0 +1,52 @@
+package com.m4l0n.hitchride.controller;
+
+
+import com.m4l0n.hitchride.exceptions.HitchrideException;
+import com.m4l0n.hitchride.pojos.DriverJourney;
+import com.m4l0n.hitchride.response.Response;
+import com.m4l0n.hitchride.response.ResponseAPI;
+import com.m4l0n.hitchride.service.DriverJourneyService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SecurityRequirement(name = "bearerAuth")
+@RequestMapping(value = "/driverJourney", produces = MediaType.APPLICATION_JSON_VALUE)
+public class DriverJourneyController {
+
+    private final DriverJourneyService driverJourneyService;
+
+    public DriverJourneyController(DriverJourneyService driverJourneyService) {
+        this.driverJourneyService = driverJourneyService;
+    }
+
+    @PostMapping("/createDriverJourney")
+    public Response postNewDriverJourney(@RequestBody DriverJourney driverJourney) {
+
+        try {
+            DriverJourney newDriverJourney = driverJourneyService.createDriverJourney(driverJourney);
+
+            return ResponseAPI.positiveResponse(newDriverJourney);
+        } catch (Exception e) {
+            throw new HitchrideException(e.getMessage());
+        }
+
+    }
+
+    @PostMapping("/acceptDriverJourney")
+    public Response acceptDriverJourney(@RequestBody DriverJourney driverJourney) {
+
+        try {
+            DriverJourney newDriverJourney = driverJourneyService.acceptDriverJourney(driverJourney);
+
+            return ResponseAPI.positiveResponse(newDriverJourney);
+        } catch (Exception e) {
+            throw new HitchrideException(e.getMessage());
+        }
+
+    }
+}
